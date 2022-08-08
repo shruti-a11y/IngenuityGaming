2d3b872fdf101c34bdc815a264a7018e41f3cb8c
shruti-a11y <shruti.sharma_cs19@gla.ac.in>
2022-08-08 09:43:15 +0530

    update

---
+++ Assignment/assign3.js
@@ -0,0 +1,10 @@
+// function myFunction() {
+//     let x = document.getElementById('myTopnav');
+//     if(x.className ==="menu"){
+//         x.className += " responsive";
+//     }
+//     else{
+//         x.className = "menu"
+//     }
+// }
+

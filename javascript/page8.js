f08ec1cfcd4b4f31dc5745eee3008bebec1da477
shruti-a11y <shruti.sharma_cs19@gla.ac.in>
2022-08-09 13:34:57 +0530

    upload

---
+++ javascript/page8.js
@@ -0,0 +1,52 @@
+   // document.getElementsByTagName('p').innerHTML = 'hello world';
+        // variable in js
+
+
+        // JS MODE:
+        // 1.Slopoy
+        // 2.Strict
+
+        "use strict";
+
+
+
+        var fname = 'Shruti';
+        let lname = 'Sharma';
+
+        // we cannot re define let variable again like var keyword
+        // as it is a block -scope varible
+        var fname ='Shreya';
+
+        console.log(typeof(fname));
+        // let lname = 'Sharma'
+
+        // data type:
+        //  primitive
+        // 1.STRING
+        // 2.NUMBER
+        // 3.BOOLEAN
+        // 4.UNDEFINE
+        // 5.NULL
+        console.log(typeof(address));
+        let trainee = null;
+
+        lname =15222;
+        console.log(lname);
+        console.log(trainee);
+
+
+        // isHoliday = 'Sunday';
+        // var isHoliday;
+
+
+        // dialog box
+        // 1.alert - display msg as Notification
+        // 2.prompt - to take input form user
+        // 3.confirm - take boolean input in yes/no
+
+        let name  = prompt('What is your name?');
+        let res = confirm('Are u confirm to delete it?');
+        alert("thank you for ur response");
+
+
+        // type conversion

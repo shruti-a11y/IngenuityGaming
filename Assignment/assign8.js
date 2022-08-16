72a41cdb9e7700cfbb73280f9842be51e70f6112
shruti-a11y <shruti.sharma_cs19@gla.ac.in>
2022-08-16 17:58:23 +0530

    update

--- Assignment/assign8.js
+++ Assignment/assign8.js
@@ -1,74 +1,68 @@
 function myFunction() {
-   
-    alert("Ques 1 : Write code to remove extra white space in between the string ");
-    let string = prompt("Enter any string with extra spaces");
-    alert("Answer : "+string.trim());
+  alert(
+    "Ques 1 : Write code to remove extra white space in between the string "
+  );
+  let string = prompt("Enter any string with extra spaces");
+  alert("Answer : " + string.trim());
 }
 function myFunction2() {
-    
-    alert("Ques 2 : Write a code to Copy an array into another using forEach()");
-    var number =prompt("Enter element seprated by space");
-    // let ans = Array.from(number);
-    let ans = number.split(" ");
-    // copy string to another
-    let res = [];
-    ans.forEach(myCopy);
-    function myCopy(item) {
-        res.push(item);
-    }
-    alert("Your string : "+ ans  + "  Copy Array : " + res);
+  alert("Ques 2 : Write a code to Copy an array into another using forEach()");
+  var number = prompt("Enter element seprated by space");
+  // let ans = Array.from(number);
+  let ans = number.split(" ");
+  // copy string to another
+  let res = [];
+  ans.forEach(myCopy);
+  function myCopy(item) {
+    res.push(item);
+  }
+  alert("Your string : " + ans + "  Copy Array : " + res);
 }
 function myFunction3() {
-    
-    alert("Ques 3 : Write a code in JavaScript to Check all Students passed ");
-    // let string = prompt("Enter any string with extra spaces");
-    let size = prompt("Enter the size of array");
-    let res = [];
-    let sum = 0;
-    while (size-- > 0 ) {
-        var num = Number(prompt("Enter number"));
-        checkNum(num);
-        sum = sum+num;
-    }  
+  alert("Ques 3 : Write a code in JavaScript to Check all Students passed ");
+  // let string = prompt("Enter any string with extra spaces");
+  let size = prompt("Enter the size of array");
+  let res = [];
+  let sum = 0;
+  while (size-- > 0) {
+    var num = Number(prompt("Enter number"));
+    checkNum(num);
+    sum = sum + num;
+  }
 
-    function checkNum(num) {
-        while (!/^[0-9]+$/.test(num)) {
-            alert("You did not enter a number.");
-            answer = prompt("Enter a number: ");
-        }
-        res.push(num);
-        alert("Number added to array");
+  function checkNum(num) {
+    while (!/^[0-9]+$/.test(num)) {
+      alert("You did not enter a number.");
+      answer = prompt("Enter a number: ");
     }
-    alert("Given Array : "+ res);
-    sum= sum/size;
-    let marksCriteria = prompt("Enter the passing criteria");
-    if (sum >= marksCriteria ) {
-        alert("Students passed"); 
-    }
-    else{
-        alert("Students failed"); 
-    }
-
+    res.push(num);
+    alert("Number added to array");
+  }
+  alert("Given Array : " + res);
+  sum = sum / size;
+  let marksCriteria = prompt("Enter the passing criteria");
+  if (sum >= marksCriteria) {
+    alert("Students passed");
+  } else {
+    alert("Students failed");
+  }
 }
 function myFunction4() {
-    
-    alert("Ques 4 : Write a code to add an Element at a particular List inside an array");
-    var number =prompt("Enter element seprated by space");
-    let ans = Array.from(number);
-    alert("Current array : "+number);
-    var item =prompt("Enter new element");
-    ans.push(item);
-    alert("New array : "+ ans);
-    
+  alert(
+    "Ques 4 : Write a code to add an Element at a particular List inside an array"
+  );
+  var number = prompt("Enter element seprated by space");
+  let ans = Array.from(number);
+  alert("Current array : " + number);
+  var item = prompt("Enter new element");
+  ans.push(item);
+  alert("New array : " + ans);
 }
 
 function myFunction5() {
-   
-    alert("Ques 5 : Write a code to sort the string in descending order.");
-    let string = prompt("Enter any string");
-    let ans =Array.from(string);
-    ans.sort();
-    alert("Answer : "+ ans);
-    
+  alert("Ques 5 : Write a code to sort the string in descending order.");
+  let string = prompt("Enter any string");
+  let ans = Array.from(string);
+  ans.sort();
+  alert("Answer : " + ans);
 }
-
--- Assignment/assign9.js
+++ Assignment/assign9.js
@@ -1,37 +1,46 @@
 function myFunction1() {
   alert(
-  "Ques 1 : Write a Program to return sum of all Arguments passed to a Function");
+    "Ques 1 : Write a Program to return sum of all Arguments passed to a Function"
+  );
   let sum = 0;
   let arr = [];
-alert("To stop the input enter 0")
-while (true) {
-	let num = parseInt(prompt("Enter a Value", "0"), 10);
-    if(num<1) break;
+  alert("To stop the input enter 0");
+  while (true) {
+    let num = parseInt(prompt("Enter a Value", "0"), 10);
+    if (num < 1) break;
     arr.push(num);
-    sum = sum+ num;
-}
-document.getElementById('card').style.display = 'block';
-document.getElementById('ques').innerHTML = "Ques 1 : Write a Program to return sum of all Arguments passed to a Function";
-document.getElementById('userInput').innerHTML = "Entered Array is : "+arr +"<br>";
-document.getElementById('answer').innerHTML = "Answer  : "+sum;
-  
+    sum = sum + num;
+  }
+  document.getElementById("card").style.display = "block";
+  document.getElementById("ques").innerHTML =
+    "Ques 1 : Write a Program to return sum of all Arguments passed to a Function";
+  document.getElementById("userInput").innerHTML =
+    "Entered Array is : " + arr + "<br>";
+  document.getElementById("answer").innerHTML = "Answer  : " + sum;
 }
 function myFunction2() {
   alert(
     "Ques 2 : Write a Program to return more than one value from a Function. "
   );
-  function getName (){
-    let fname = prompt("Enter First Name : ")
-    let lname = prompt("Enter last Name : ")
-    return {fname, lname};
-};
-let name = getName();
-alert("Hii " + name.fname + " " + name.lname + " nice To meet you ;> ");
-document.getElementById('card').style.display = 'block';
-document.getElementById('ques').innerHTML = "Ques 2 : Write a Program to return more than one value from a Function. ";
-document.getElementById('userInput').innerHTML = "Enter First Name : "+name.fname+"<br>"+"Enter Last Name : "+name.lname+"<br>";
-document.getElementById('answer').innerHTML = "Hii " + name.fname + " " + name.lname + " nice To meet you ;> "
-
+  function getName() {
+    let fname = prompt("Enter First Name : ");
+    let lname = prompt("Enter last Name : ");
+    return { fname, lname };
+  }
+  let name = getName();
+  alert("Hii " + name.fname + " " + name.lname + " nice To meet you ;> ");
+  document.getElementById("card").style.display = "block";
+  document.getElementById("ques").innerHTML =
+    "Ques 2 : Write a Program to return more than one value from a Function. ";
+  document.getElementById("userInput").innerHTML =
+    "Enter First Name : " +
+    name.fname +
+    "<br>" +
+    "Enter Last Name : " +
+    name.lname +
+    "<br>";
+  document.getElementById("answer").innerHTML =
+    "Hii " + name.fname + " " + name.lname + " nice To meet you ;> ";
 }
 
 // Write a code to return entire function definition and display it.
@@ -40,16 +49,16 @@ function myFunction3() {
   alert(
     "Ques 3 : Write a code to return entire function definition and display it. "
   );
-  function add(a,b){
-    let sum = a+b;
+  function add(a, b) {
+    let sum = a + b;
     return add;
   }
   alert(add());
-  document.getElementById('card').style.display = 'block';
-document.getElementById('ques').innerHTML = "Ques 3 : Write a code to return entire function definition and display it. ";
-document.getElementById('answer').innerHTML = add();
-  
-  
+  // alert("ans :"+ add.toString() );  // to display function definition.
+  document.getElementById("card").style.display = "block";
+  document.getElementById("ques").innerHTML =
+    "Ques 3 : Write a code to return entire function definition and display it. ";
+  document.getElementById("answer").innerHTML = add();
 }
 
 // Write a JavaScript program to create a new string adding "New!" in front of a given String. If the given string begins with "New!" already then return the original string. (Use Functional Approach)
@@ -62,17 +71,17 @@ function myFunction4() {
   let res = string.startsWith("New");
   ans = string;
   if (res != true) {
-    ans = "New!!! " + string
-    alert( ans);
+    ans = "New!!! " + string;
+    alert(ans);
   } else {
     alert(ans);
   }
 
-  document.getElementById('card').style.display = 'block';
-  document.getElementById('ques').innerHTML = 'Ques 4 : Write a JavaScript program to create a new string adding "New!" in front of a given String. If the given string begins with "New!" already then return the original string. ';
-  document.getElementById('userInput').innerHTML = "Entered string : "+ string;
-  document.getElementById('answer').innerHTML = "Answer : " + ans;
-    
+  document.getElementById("card").style.display = "block";
+  document.getElementById("ques").innerHTML =
+    'Ques 4 : Write a JavaScript program to create a new string adding "New!" in front of a given String. If the given string begins with "New!" already then return the original string. ';
+  document.getElementById("userInput").innerHTML = "Entered string : " + string;
+  document.getElementById("answer").innerHTML = "Answer : " + ans;
 }
 
 // Write a JavaScript program to create a new string from a given string taking first 3 characters and the last 3 characters of a string and add them together. The string length must be 3 or more. If not, return the original string is returned.  (Use Functional Approach)
@@ -82,30 +91,25 @@ function myFunction5() {
     "Ques 5 : Write a JavaScript program to create a new string from a given string taking first 3 characters and the last 3 characters of a string and add them together. The string length must be 3 or more. If not, return the original string is returned.  "
   );
   let string = prompt("Enter any string");
-  if(string.length<3){
+  if (string.length < 3) {
     return string;
+  } else {
+    ans = conbineString(string);
+    function conbineString(text) {
+      let first_part = text.substring(0, 3);
+      let last_part = text.substring(text.length, text.length - 3);
+      return first_part + last_part;
+    }
+    alert(ans);
   }
-  else{
-      ans = conbineString(string);
-      function conbineString(text) {
-        let first_part = text.substring(0,3);
-        let last_part = text.substring(text.length,text.length-3);
-        return (first_part+last_part);
-      }
-      alert(ans);
 
-  } 
-  
-  document.getElementById('card').style.display = 'block';
-  document.getElementById('ques').innerHTML = 'Ques 5 : Write a JavaScript program to create a new string from a given string taking first 3 characters and the last 3 characters of a string and add them together. The string length must be 3 or more. If not, return the original string is returned. ';
-  document.getElementById('userInput').innerHTML = "Entered string : "+ string;
-  document.getElementById('answer').innerHTML = "Answer : " + ans;
+  document.getElementById("card").style.display = "block";
+  document.getElementById("ques").innerHTML =
+    "Ques 5 : Write a JavaScript program to create a new string from a given string taking first 3 characters and the last 3 characters of a string and add them together. The string length must be 3 or more. If not, return the original string is returned. ";
+  document.getElementById("userInput").innerHTML = "Entered string : " + string;
+  document.getElementById("answer").innerHTML = "Answer : " + ans;
 }
 
-
-function goBack(){
-  document.getElementById('card').style.display = 'none';
+function goBack() {
+  document.getElementById("card").style.display = "none";
 }
-
-
-
